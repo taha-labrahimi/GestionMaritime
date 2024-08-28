5d38838083ab244c96f5b22174ec841736a9795f
TAHA LABRAHIMI <144866467+taha-labrahimi@users.noreply.github.com>
2024-08-28 01:04:45 +0100

    adding gestion maritime logic (update)

--- repositories/NavireRepository.js
+++ repositories/NavireRepository.js
@@ -1,12 +1,23 @@
 const Navire = require('../models/Navire');
+const Port = require('../models/Port');
 
 class NavireRepository {
     async findAll() {
-        return await Navire.findAll();
+        return await Navire.findAll(
+            {
+                include: [
+                    { model: Port }
+                ]
+            }
+        );
     }
 
     async findById(id) {
-        return await Navire.findByPk(id);
+        return await Navire.findByPk(id,{
+            include: [
+                { model: Port }
+            ]
+        });
     }
 
     async create(navireData) {
